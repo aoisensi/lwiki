c3d5785a9b1580c6d9a0cdc8fc598f1979ebd616
aoisensi <aoisensi@aoisensi.info>
2013-07-30 23:47:54 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lwiki::Application.config.secret_key_base = 'a43549172ac34cf63311624b4ad3f0204b7e77f749de78366ae070537abb61a9a1334b1949209bc49242e654369939addd3c0b210e0f4cdbf8be12811c1f955a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lwiki::Application.config.session_store :cookie_store, key: '_lwiki_session'
